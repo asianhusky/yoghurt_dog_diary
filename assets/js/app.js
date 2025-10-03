53b05fe7298f2be5aaffce59d5537fc249d87332
asianhusky <156965207+asianhusky@users.noreply.github.com>
2025-10-02 17:29:15 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,7 @@ const dlBtn = $('#downloadJson');
 const upInput = $('#uploadJson');
 const schemaEl = $('#schema');
 
-let DATA = { entries: [] };
-let chart;
+let DATA = { entries: [] }
 
 init();
 
